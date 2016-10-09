b927acef1883b7584977a5ef057f38dc01644abc
jie.fu <fujie1988@gmail.com>
2016-10-10 03:55:44 +0800

    update

--- maker/ks_maker.py
+++ maker/ks_maker.py
@@ -3,7 +3,7 @@ from jinja2 import Environment, PackageLoader
 from utils import ks_manager as km
 
 if __name__ == "__main__":
-    env = Environment(loader=PackageLoader('post', 'templates'))
+    env = Environment(loader=PackageLoader('page', 'templates'))
     template = env.get_template('ks_project/base.html')
     for page in km.fetch_crawler_data():
         print template.render(page)
